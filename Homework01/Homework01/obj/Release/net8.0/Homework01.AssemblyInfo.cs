b4b3c78e7b04f0085a7e6242bcd0f96ade43a80b
Lisanna <gerashchenko.alisa@student.ehu.lt>
2025-04-08 12:58:23 +0300

    bebe

--- Homework01/Homework01/obj/Release/net8.0/Homework01.AssemblyInfo.cs
+++ Homework01/Homework01/obj/Release/net8.0/Homework01.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Homework01")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+742a51d42d6b5c0de3460c3da323f75ac3580ad1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Homework01")]
